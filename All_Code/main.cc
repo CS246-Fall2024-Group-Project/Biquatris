e03c83ebf856b17b4733a3b08033bbc407b301e4
jmccrank <jmccrank@uwaterloo.ca>
2024-11-23 15:03:06 -0500

    Last commit before I go to the gym.

--- All_Code/main.cc
+++ All_Code/main.cc
@@ -9,17 +9,24 @@ void welcomeMessage() {
 
 int main() {
     welcomeMessage();
+    // creating the canvas for the players board
     Canvas game_board1(18, 11);
     Canvas game_board2(18, 11);
+
     Queue queue1, queue2;
+
     Player player1("Player 1", queue1);
     Player player2("Player 2", queue1);
 
     bool gameOver = false;
     cout << "Player 1, please choose your starting level: ";
+
     player1.chooseLevel();
+    
     cout << "Player 2, please choose your starting level: ";
     player2.chooseLevel();
+
+    // Loop while the game is playing
     while (!gameOver) {
         cout << "Player 1's turn: " << endl;
         player1.takeTurn(game_board1);
@@ -27,6 +34,8 @@ int main() {
         if (player1.gameOver()) {
             gameOver = true;
         }
+
+        //queue.nextShape(); // MAKE QUEUE GET NEXT SHAPE
 
         cout << "Player 2's turn: " << endl;
         player2.takeTurn(game_board2);
